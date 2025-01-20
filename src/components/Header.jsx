e65fcde1437cb283501a91c659fce75294fde9f9
Ruhul Mirza <135419108+Ruhul-Mirza@users.noreply.github.com>
2025-01-20 18:06:53 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ const Header = () => {
         {/* Button */}
 
         <NavLink to="/contact"> 
-        <button type="button" className="py-2 px-3.5 text-sm font-medium focus:outline-none rounded-full border border-gray-200 text-white bg-blue-700/80 hover:bg-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Reach Out</button>
+        <button className="text-sm px-3 py-1.5 hover:bg-blue-600  rounded-full bg-blue-600/80 text-white">Reach Out</button>
 
         </NavLink>
       </div>
